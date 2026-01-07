15e9a0c383ec226405a2cdeb3798675320ce26b6
Jaishuk <jaishukreddy7@gmail.com>
2026-01-07 14:08:23 +0530

    step 1 main.py.

---
+++ main.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import sync_playwright
+from datetime import datetime
+import os
+
+BRAVE_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+USER_DATA_DIR = r"C:\Users\jaish\AppData\Local\BraveSoftware\Brave-Browser\User Data"
+
+SCREENSHOT_DIR = "screenshots"
+os.makedirs(SCREENSHOT_DIR, exist_ok=True)
+
+with sync_playwright() as p:
+    context = p.chromium.launch_persistent_context(
+        user_data_dir=USER_DATA_DIR,
+        executable_path=BRAVE_PATH,
+        channel="chrome",
+        headless=False,
+        args=[
+            "--profile-directory=Default",
+            "--disable-blink-features=AutomationControlled"
+        ]
+    )
+    
+    page = context.new_page()
+    page.goto("https://www.google.com", timeout=600000)
+    
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    screenshot_path = f"{SCREENSHOT_DIR}/step1_test_{timestamp}.png"
+    page.screenshot(path=screenshot_path)
+    
+    print("Screenshot saved: ", screenshot_path)
+    
+    input("Press ENTER to close the browser...")
+    context.close()
+    
+    
